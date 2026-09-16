fb0feea5fa6e8e8cdc041db245388fcaaf6d9bfa
Shivani Modi <143shivani05@gmail.com>
2026-09-16 07:22:58 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,18 +2,47 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hi", (req,res) => {
-    res.send("Helloo from the dashboard!");
-    next();
+////// ORDERRRRRRRRRRRRR MATTERSSSSSSSSSSSSSSSSSSS
+
+
+app.get("/users", (req,res) => {
+    console.log(req.query);
+    res.send("Done");
 })
 
-app.use("/hello", (req,res) => {
-    res.send("finally server created!");
-    next();
+app.get("/users/:id", (req,res) => {
+    console.log(req.params);
+    res.send("Done");
 })
 
-app.use("/test", (req,res) => {
-    res.send("heyy from server!");
-})
+// app.use("/hi", (req,res) => {
+//     res.send("Helloo from the dashboard!");
+// })
+
+// app.use("/user", (req,res) => {
+//     res.send("HaHAAAAAAAAAAAA");
+// })
+
+// this will handle only GET calls to /user
+// app.get("/user",(req,res) => {
+//       res.send({firstName:"Shivani", lastName:"Modi"});
+// });
+
+// app.post("/user", (req, res) => {
+//     res.send("Data saved to the database");
+// })
+
+// app.delete("/user", (req, res) => {
+//     res.send("Deleted successfully");
+// })
+
+// this will handle all the http methods
+// app.use("/hello", (req,res) => {
+//     res.send("finally server created!");
+// })
+
+// app.use("/test", (req,res) => {
+//     res.send("heyy from server!");
+// })
 
 app.listen(3000);

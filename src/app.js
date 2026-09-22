76c21b82733a5f16446db042f4c5d7072ec77fcb
Shivani Modi <143shivani05@gmail.com>
2026-09-22 12:33:25 +0530

    Added MongoDB environment configuration

--- src/app.js
+++ src/app.js
@@ -233,7 +233,7 @@ app.use(express.json());
 
 app.post("/signup" , async (req,res) => {
      const user = new User(req.body);
-     
+
     try {
      await user.save();
      res.send("User added successfully");
@@ -241,6 +241,24 @@ app.post("/signup" , async (req,res) => {
     catch (err) {
       res.status(400).send("Error saving the user:" + err.message);
     }
+})
+
+// get user by email
+app.get("/user", async (req,res) => {
+     const userEmail =req.body.emailId;
+
+     try {
+   const User =  await  User.find({emailId: userEmail});
+   res.send(user);
+     }
+   catch(err) {
+     res.status(400).send("Something went wrong");
+   }
+})
+
+// feed api - GET - get all the users from the database
+app.get("/feed", (req,res) => {
+
 })
 
 connectDB() 
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     await mongoose.connect(
-       "mongodb+srv://whoamiguess935_db_user:shivanimodi@hellonode.vvymowz.mongodb.net/DevNexus"
+       process.env.MONGO_URI
     );
 }
 

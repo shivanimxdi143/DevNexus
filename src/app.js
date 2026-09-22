b863758c388766350253c065a2aa7c57ead429b6
Shivani Modi <143shivani05@gmail.com>
2026-09-22 11:59:11 +0530

    dynamic data in database api

--- src/app.js
+++ src/app.js
@@ -191,18 +191,49 @@
 
 ///// creating up a api
 
+// const express = require("express");
+// const connectDB = require("./config/database");
+// const app = express();
+// const User = require("./models/user");
+
+// app.post("/signup" , async (req,res) => {
+//      const user = new User({
+//       firstName: "Shivani",
+//       lastName: "Modi",
+//       emailId: "shivani123@gmail.com",
+//       password: "shivani1234@",
+//      });
+//     try {
+//      await user.save();
+//      res.send("User added successfully");
+//     }
+//     catch (err) {
+//       res.status(400).send("Error saving the user:" + err.message);
+//     }
+// })
+// connectDB()
+//      .then(() => {
+//       console.log("DataBase connection established");
+//       app.listen(3000);
+// })
+//      .catch((err) => {
+//         console.log("Database can't be connected");
+//      })
+
+
+
+///// diving directly into apis
+
 const express = require("express");
 const connectDB = require("./config/database");
-const app = express();
 const User = require("./models/user");
+const app = express();
+
+app.use(express.json());
 
 app.post("/signup" , async (req,res) => {
-     const user = new User({
-      firstName: "Shivani",
-      lastName: "Modi",
-      emailId: "shivani123@gmail.com",
-      password: "shivani1234@",
-     });
+     const user = new User(req.body);
+     
     try {
      await user.save();
      res.send("User added successfully");
@@ -211,12 +242,12 @@ app.post("/signup" , async (req,res) => {
       res.status(400).send("Error saving the user:" + err.message);
     }
 })
-connectDB()
-     .then(() => {
-      console.log("DataBase connection established");
-      app.listen(3000);
-})
-     .catch((err) => {
-        console.log("Database can't be connected");
-     })
 
+connectDB() 
+  .then(() => {
+     console.log("Connection established successfully");
+     app.listen(3000);
+  })
+  .catch((err) => {
+     console.log("Database can't be connected");
+  })
--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     await mongoose.connect(
-       "mongodb+srv://whoamiguess935_db_user:Shivani1234@hellonode.vvymowz.mongodb.net/DevNexus"
+       "mongodb+srv://whoamiguess935_db_user:shivanimodi@hellonode.vvymowz.mongodb.net/DevNexus"
     );
 }
 
 module.exports = connectDB;
+
 
